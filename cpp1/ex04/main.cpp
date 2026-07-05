fee5e77528956362c8749287cbbd94117b80132a
llugez <llugez@f6r3s10.paris.42.school>
2026-07-05 21:41:09 +0200

    ok

--- cpp1/ex04/main.cpp
+++ cpp1/ex04/main.cpp
@@ -1,6 +1,26 @@
+#include <ios>
+#include <fstream>
 #include <iostream>
+
+/*
+	std::fstream fs(argv[1], std::ios::in); // Ouverture pour la lecture
+	fs_src << "Hello World" // Erreur siliencieuse
+*/
 
 int main(int argc, char *argv[])
 {
-	
+	if (argc == 4)
+	{
+		std::string replace_file = std::string(argv[1]) + ".replace";
+		std::fstream fs_src(argv[1], std::ios::out);
+		// File qui existe pas 
+		// File qui n'a pas les perm
+		std::fstream fs_dest(replace_file.c_str(), std::ios::out);
+		fs_src << "hello les gensssssssssss";
+
+		// pas oublier de close les fstream
+		return (0);
+	}
+	std::cout << "invalide nb argument\n";
+	return (1);
 }
---
+++ playground/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <istream>
+
+int main()
+{
+	std::string str;
+	std::cin >> str;
+	std::istream v();
+}
